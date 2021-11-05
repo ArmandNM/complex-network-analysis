4509ee50777672eda81dffae8dfda26bd7dc0202
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2021-11-05 03:24:09 +0200

    Add breadth first search

--- Lab 1/basic_algorithms.py
+++ Lab 1/basic_algorithms.py
@@ -1,5 +1,7 @@
 import pprint
 
+from queue import Queue
+
 from synthetic_data import SyntheticGraphGenerator
 
 
@@ -17,11 +19,27 @@ def depth_first_search(graph, starting_node):
 
     _dfs(starting_node, graph, visited, output)
 
-    return output
+    return visited, output
 
 
-def breadth_first_search(graph):
-    pass
+def breadth_first_search(graph, starting_node):
+    visited = set()  # no nodes yet visited
+    output = []  # order of visiting nodes
+    q = Queue()
+
+    q.put(starting_node)
+    visited.add(starting_node)
+
+    while not q.empty():
+        node = q.get()
+        output.append(node)
+
+        for neigh in graph.get_neighbours(node):
+            if neigh not in visited:
+                visited.add(neigh)
+                q.put(neigh)
+
+    return visited, output
 
 
 def get_connected_components(graph):
@@ -46,8 +64,11 @@ def main():
     random_graph = SyntheticGraphGenerator.create_random_edge_graph(num_nodes=5, edge_prob=0.5)
     pp.pprint(random_graph.adjacency_list)
 
-    res = depth_first_search(random_graph, next(iter(random_graph.nodes)))
-    print(res)
+    _, out = depth_first_search(random_graph, next(iter(random_graph.nodes)))
+    print(out)
+
+    _, out = breadth_first_search(random_graph, next(iter(random_graph.nodes)))
+    print(out)
 
 
 if __name__ == '__main__':
