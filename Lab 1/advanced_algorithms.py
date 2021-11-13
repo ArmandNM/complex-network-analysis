0b806b49a72eca2c436467eea4db474bf701feff
Armand Nicolicioiu <armand.nicolicioiu@gmail.com>
2021-11-13 03:39:31 +0200

    Add clustering coefficients

--- Lab 1/advanced_algorithms.py
+++ Lab 1/advanced_algorithms.py
@@ -1,6 +1,7 @@
 import pprint
 
 from synthetic_data import SyntheticGraphGenerator
+from basic_algorithms import compute_degree_sequence
 
 
 def count_triangles(graph):
@@ -22,12 +23,49 @@ def count_triangles(graph):
     return num_triangles // 6
 
 
+def clustering_coefficients(graph):
+    coeff = {}
+
+    # Compute the degree sequence
+    degree = compute_degree_sequence(graph)
+
+    # Run a depth 2 DFS from each node
+    for start_node in graph.nodes:
+        num_triangles = 0
+        for neigh1 in graph.get_neighbours(start_node):
+            for neigh2 in graph.get_neighbours(neigh1):
+                # Don't return to parent
+                if neigh2 == start_node:
+                    continue
+                for neigh3 in graph.get_neighbours(neigh2):
+                    # If returned to start_node, we found a triangle
+                    if neigh3 == start_node:
+                        num_triangles += 1
+
+        # Each triangle was counted twice
+        num_triangles = num_triangles // 2
+
+        # Compute the theoretical maximum possible number of triangles
+        max_triangles = degree[start_node] * (degree[start_node] - 1) / 2
+
+        if max_triangles > 0:
+            coeff[start_node] = num_triangles / max_triangles
+        else:
+            coeff[start_node] = 0
+
+    return coeff
+
+
 def main():
     pp = pprint.PrettyPrinter(indent=4)
 
     graph = SyntheticGraphGenerator.create_random_edge_graph(num_nodes=5, edge_prob=0.5)
     pp.pprint(graph.adjacency_list)
     print(f'Random graph: num_traingles = {count_triangles(graph)}')
+
+    coeff = clustering_coefficients(graph)
+    print('Random graph: clustering coefficients')
+    pp.pprint(coeff)
 
 
 if __name__ == '__main__':
